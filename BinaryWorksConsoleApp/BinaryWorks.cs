b3f74a126d2726ea4b9d56e49b16e1458c3f4c5e
Marco Duren Centeno <marco-centeno@procergs.rs.gov.br>
2023-03-16 11:05:45 -0300

    Add project files.

---
+++ BinaryWorksConsoleApp/BinaryWorks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryWorksConsoleApp
+{
+    public class BinaryWorks
+    {
+        public string ToBinary(int val)
+        {
+            //Console.Write(val + " - ");
+            string output = "";
+            do
+            {
+                int resto = val % 2;
+                output = resto.ToString() + output;
+                val = val / 2;
+            } while (val > 0);
+            //Console.WriteLine(output);
+            return output;
+        }
+
+        public int Cardinality(string bNum, char alg)
+        {
+            int card = 0;
+            foreach (char c in bNum)
+            {
+                if (c == alg)
+                {
+                    card++;
+                }
+            }
+            return card;
+        }
+    }
+}
---
+++ BinaryWorksConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using BinaryWorksConsoleApp;
+
+BinaryWorks binaryWorks = new BinaryWorks();
+
+for (int i = 0; i < 33; i++)
+{
+    Console.WriteLine($"{i} - {binaryWorks.ToBinary(i)} - {binaryWorks.Cardinality(binaryWorks.ToBinary(i), '1')}");
+}
